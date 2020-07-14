6a112924c819da39a04b174a06a78591ea53664c
nithintsk <nithintsk@github.com>
2020-07-14 04:42:36 -0400

    Set up the flask endpoint to respond to frontend. Now have to create frontend to display results.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    displayResults = (matchingSeriesList) => {
+        console.log(matchingSeriesList);
+    };
+
+    render() {
+    return (
+         <div className="App">
+             <header className="App-header">
+               <h1>IMDb TV ratings visualizer</h1>
+               <Form onSubmit={this.displayResults} />
+               <img src={logo} className="App-logo" alt="logo" />
+               <p>
+                 Compare the ratings of episodes from S01E01 to the finale using a color coded heatmap
+               </p>
+         {/*<a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+             >
+               Learn React
+             </a>*/}
+             </header>
+        </div>
+        );
+    };
 }
 
 export default App;
